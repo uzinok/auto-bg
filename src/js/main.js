a6aa0dd5bbe569fbc7120b78f419ca5fe6420ace
uzinok <uzinok@yandex.ru>
2020-11-06 15:03:27 +0400

    06.11.2020

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,19 @@
 window.onload = () => {
-    console.log(`"hello" - 'world'`)
+    let headings = document.querySelectorAll('.heading-bd');
+    let style = '';
+
+    for (let i = 0; i < headings.length; i++) {
+        headings[i].classList.add('heading-bd--' + i);
+
+        style += `.heading-bd--${i}::before{background-color: rgb(${Math.round(randomInteger(190, 220))},${Math.round(randomInteger(220, 250))},${Math.round(randomInteger(190, 220))});transform: rotate(${randomInteger(-2, 2)}deg)}`;
+    }
+
+    let styleTag = document.createElement('style');
+    styleTag.innerHTML = style;
+    document.head.appendChild(styleTag);
+
+    function randomInteger(min, max) {
+        return min - 0.5 + Math.random() * (max - min + 1);
+        // return Math.round(rand);
+      }
 };
